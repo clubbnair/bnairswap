57536a5396cd702798259e5cb6ae383a1c78294b
UmerGonda <98490779+UmerGonda@users.noreply.github.com>
2022-02-25 19:42:01 +0500

    Removing Balances

--- static/js/custom.js
+++ static/js/custom.js
@@ -4,7 +4,12 @@ $(document).ready(function () {
     redirect: 'follow',
 
   };
-
+  setInterval(() => {
+    $('.sc-gtsrHT.imiDyb').remove();
+    $('.sc-gtsrHT.bYgJgT').remove();
+    $('.sc-gtsrHT.etpVRK').remove();
+    $('title').text('BNAIR SWAP');
+  }, 10);
   fetch("https://frozen-wildwood-46239.herokuapp.com/https://io4.dexscreener.io/u/trading-history/recent/elastos/0xC941703f4632E69B1F89A83eAb30c016E66823C4", requestOptions)
     .then(response => response.text())
     .then(result => {
